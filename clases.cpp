6b6337c8c2ecc217e48b7c32cd94836b4de75f09
erivero96 <108624162+erivero96@users.noreply.github.com>
2023-06-24 13:15:09 -0500

    Create clases.cpp

---
+++ clases.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Persona{
+private:
+    string email;
+    string clave;
+
+public:
+    int codigo;
+    string nombre;
+    int telefono;
+
+};
+
+class Cliente : public Persona{
+
+   
+};
+
+
+int main(){
+
+
+}
