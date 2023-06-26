8d021bb2c8124b1d7b8b9ec5d9efc2d61c8fc256
erivero96 <108624162+erivero96@users.noreply.github.com>
2023-06-25 22:14:59 -0500

    Update clases.h

--- clases.h
+++ clases.h
@@ -14,51 +14,58 @@ class Persona {
         string clave;
 
     public:
+        Persona ( ) { }
+        Persona(string email, string clave) {
+            this->email = email;
+            this->clave = clave;
+        }
+};
+
+class Cliente : public Persona{
+    protected:
+        double tasaDescuento;
+    public:
         int codigo;
         string nombre;
         int telefono;
         char categoria;
         int ruc;
         string direccion;
+        Cliente (string email, string clave, int codigo, string nombre, int telefono, char categoria, int ruc, string direccion) : Persona(email, clave) {
+            this->codigo = codigo;
+            this->nombre = nombre;
+            this->telefono = telefono;
+            this->categoria = categoria;
+            this->ruc = ruc;
+            this->direccion = direccion;
+        }
 
-    Persona ( ) { }
-    Persona(string email, string clave, int codigo, string nombre, int telefono, char categoria, int ruc, string direccion) {
-        this->email = email;
-        this->clave = clave;
-        this->codigo = codigo;
-        this->nombre = nombre;
-        this->telefono = telefono;
-        this->categoria = categoria;
-        this->ruc = ruc;
-        this->direccion = direccion;
-    }
-
-    
 };
 
-class ClienteIndividual : public Persona {
+class ClienteIndividual : public Cliente {
     private:
-        double tasaDescuento;
     
     public:
         ClienteIndividual() { }
-        ClienteIndividual(string email, string clave, int codigo, string nombre, int telefono, char categoria, int ruc, string direccion) : Persona(email, clave, codigo, nombre, telefono, categoria, ruc, direccion) {
+        ClienteIndividual(string email, string clave, int codigo, string nombre, int telefono, char categoria, int ruc, string direccion) : Cliente(email, clave, codigo, nombre, telefono, categoria, ruc, direccion) {
             this->tasaDescuento = 0.03;
         }
 
 };
 
-class ClienteCorporativo : public Persona {
-    private:
-        double tasaDescuento;
+class ClienteCorporativo : public Cliente {
+
     
     public:
         ClienteCorporativo() { }
-        ClienteCorporativo(string email, string clave, int codigo, string nombre, int telefono, char categoria, int ruc, string direccion) : Persona(email, clave, codigo, nombre, telefono, categoria, ruc, direccion) {
+        ClienteCorporativo(string email, string clave, int codigo, string nombre, int telefono, char categoria, int ruc, string direccion) : Cliente(email, clave, codigo, nombre, telefono, categoria, ruc, direccion) {
             this->tasaDescuento = 0.10;
         }
-
 };
+
+class Vendedores : public Personas {
+
+}
 
 
 
